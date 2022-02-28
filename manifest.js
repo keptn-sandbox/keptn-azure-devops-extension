a73a6fb4926bdf327f6f365e425d28c52f06e572
Paolo Chila <paolo.chila@dynatrace.com>
2022-02-28 13:29:00 +0100

    build: remove package.json and node_modules from tasks directories
    Remove duplication of dependencies in each task by coalescing all task
    dependencies in the root directory.
    IDEs and node work fine with this approach but running on Azure DevOps
    agents requires each task to package its own dependencies.
    To work around this the same package.json and node_modules is copied in
    each task directory when packaging
    
    Signed-off-by: Paolo Chila <paolo.chila@dynatrace.com>

--- manifest.js
+++ manifest.js
@@ -100,14 +100,26 @@ module.exports = (env) => {
 			"path": "PrepareKeptnEnvTask"
 		  },
 		  {
+			"path": "dist", "packagePath": "/PrepareKeptnEnvTask"
+		  },
+		  {
 			"path": "SendKeptnEventTask"
+		  },
+		  {
+			"path": "dist", "packagePath": "/SendKeptnEventTask"
 		  },
 		  {
 			"path": "WaitForKeptnEventTask"
 		  },
 		  {
+			"path": "dist", "packagePath": "/WaitForKeptnEventTask"
+		  },
+		  {
 			"path": "AddKeptnResourceTask"
-		  }
+		  },
+		  {
+			"path": "dist", "packagePath": "/AddKeptnResourceTask"
+		  },
 		]
 	  }
 
